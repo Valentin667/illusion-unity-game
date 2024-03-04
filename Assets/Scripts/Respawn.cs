382183b14d2b027f9d8bab0e8a02fd3ac2953fdd
Valentin Jullien <98819990+Valentin667@users.noreply.github.com>
2024-03-04 15:01:40 +0100

    Add a key bind "R" for respawn the player

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,20 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
     public float threshold;
-
     private CharacterController m_Character;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get the CharacterController component attached to this GameObject
+        // Assuming the CharacterController is on the same GameObject as this script
         m_Character = GetComponent<CharacterController>();
         if (m_Character == null)
         {
+            // Log a warning if the CharacterController component is not found
             Debug.LogWarning("CharacterController component not found on the GameObject.");
         }
     }
@@ -22,12 +20,16 @@ public class Respawn : MonoBehaviour
     // Called once per frame
     void Update()
     {
-        // Check if the CharacterController is not null and if the object's position is below the threshold
         if (m_Character != null && transform.position.y < threshold)
         {
             m_Character.Move(new Vector3(0.32f, 1f, -2.49f) - transform.position);
-
             Debug.Log("Respawn!");
-        }   
+        }
+
+        // Detect when the "R" key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_Character.Move(new Vector3(0.32f, 1f, -2.49f) - transform.position);
+        }
     }
 }
